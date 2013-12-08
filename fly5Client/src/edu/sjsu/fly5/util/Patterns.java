d7d3a37fcad77adc737356ab01dd5d1e5bc1a7f9
Vijaya <Vijaya@10.185.199.33>
2013-12-07 22:03:52 -0800

    Patterns

---
+++ fly5Client/src/edu/sjsu/fly5/util/Patterns.java
@@ -0,0 +1,8 @@
+package edu.sjsu.fly5.util;
+
+public class Patterns {
+	public static final String STRING_PATTERN = "[a-z]+";
+	public static final String TIME_PATTERN = "[1-24]:[0-60]|[1-24]";
+	public static final String INTEGER_PATTERN = "[1-9]+";
+
+}
